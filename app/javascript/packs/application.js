7c43c3a1a8e10829d6515ad5c762748cbe763ca3
ryota-5k <rk0509@hotmail.co.jp>
2021-07-04 22:08:50 +0900

    ホームのヘッダー部分、ポップアップ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,26 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+function popupSetting(){
+  let popup = document.getElementById('popup');
+  if(!popup) return;
+
+  let bgBlack = document.getElementById('bg-black');
+  let closeBtn = document.getElementById('close-btn');
+  let signUpBtn = document.getElementById('sign-up-btn');
+
+  popUp(bgBlack);
+  popUp(closeBtn);
+  popUp(signUpBtn);
+
+  function popUp(elem){
+    if(!elem) return;
+
+    elem.addEventListener('click', function(){
+      popup.classList.toggle('is-show');
+    });
+  }
+}
+
+popupSetting();
