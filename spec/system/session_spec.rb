0cef9e3c4b24ef63c3471eaeb6dc129cf71ff141
ryota-5k <rk0509@hotmail.co.jp>
2021-07-17 16:49:08 +0900

    ログイン時のテスト追加

--- spec/system/session_spec.rb
+++ spec/system/session_spec.rb
@@ -8,19 +8,19 @@ RSpec.describe "ログイン", type: :system do
       visit new_pharmacist_session_path
     end
 
-    # describe "正しい値を入力した時" do
-    #   before do
-    #     fill_in "pharmacist_email", with: pharmacist.email
-    #     fill_in "pharmacist_password", with: pharmacist.password
-    #     click_on "ログイン"
-    #   end
+    context "正しい値を入力した時" do
+      before do
+        fill_in "pharmacist_email", with: pharmacist.email
+        fill_in "pharmacist_password", with: pharmacist.password
+        click_on "ログイン"
+      end
 
-    #   it "マイページに移動すること" do
-    #     expect(current_path).to eq pharmacist_path(pharmacist)
-    #   end
-    # end
+      it "マイページに移動すること" do
+        expect(current_path).to eq pharmacist_path(pharmacist)
+      end
+    end
 
-    describe "間違った値を入力した時" do
+    context "間違った値を入力した時" do
       before do
         fill_in "pharmacist_email", with: "sample@sample.com"
         fill_in "pharmacist_password", with: pharmacist.password
@@ -29,6 +29,46 @@ RSpec.describe "ログイン", type: :system do
 
       it "ログインページに留まること" do
         expect(page).to have_content("ログイン(薬剤師)")
+      end
+
+      it "エラーメッセージが表示されること" do
+        expect(page).to have_content("違います")
+      end
+    end
+  end
+
+  describe "学生" do
+    let(:student) { create(:student) }
+
+    before do
+      visit new_student_session_path
+    end
+
+    context "正しい値を入力した時" do
+      before do
+        fill_in "student_email", with: student.email
+        fill_in "student_password", with: student.password
+        click_on "ログイン"
+      end
+
+      it "マイページに移動すること" do
+        expect(current_path).to eq student_path(student)
+      end
+    end
+
+    context "間違った値を入力した時" do
+      before do
+        fill_in "student_email", with: "sample@sample.com"
+        fill_in "student_password", with: student.password
+        click_on "ログイン"
+      end
+
+      it "ログインページに留まること" do
+        expect(page).to have_content("ログイン(学生)")
+      end
+
+      it "エラーメッセージが表示されること" do
+        expect(page).to have_content("違います")
       end
     end
   end
