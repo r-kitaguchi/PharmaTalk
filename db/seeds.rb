cd8d30766cd0ff13e960844ea0411fdcae3f13b6
ryota-5k <rk0509@hotmail.co.jp>
2021-07-20 19:09:59 +0900

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+pharmacist = Pharmacist.create!(
+  email: "sample@sample.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+pharmacist.create_pharmacist_profile!(
+  name: "田中太郎",
+  image:File.open("./public/images/sample1.png"),
+  work_place: "田中薬局",
+  work_place_type: 1,
+  work_location: 1,
+  university: "東京大学",
+  introduction: "田中です。よろしくお願いします！"
+)
+
+student = Student.create!(
+  email: "student@sample.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+student.create_student_profile!(
+  name: "佐藤次郎",
+  image:File.open("./public/images/sample2.png"),
+  university: "大阪大学",
+  year: 1,
+  introduction: "佐藤です。よろしくお願いします。"
+)
+
+20.times do |n|
+  email = Faker::Internet.email
+
+  Pharmacist.create!(
+    email: email,
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+
+Pharmacist.where(id: 2..21).each do |pharmacist|
+  name = Gimei.kanji
+  work_place_type = Faker::Number.between(from:0, to:2)
+  work_location = Faker::Number.between(from:0, to:47)
+
+  pharmacist.create_pharmacist_profile!(
+    name: name,
+    work_place: "サンプル薬局",
+    work_place_type: work_place_type,
+    work_location: work_location,
+    university: "サンプル大学",
+    introduction: "よろしくお願いします！"
+  )
+end
