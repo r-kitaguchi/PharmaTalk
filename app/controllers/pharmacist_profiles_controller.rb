ed97c4561338d98e22313fa66b9f330fa4efcb6b
ryota-5k <rk0509@hotmail.co.jp>
2021-07-21 17:48:40 +0900

    kaminariページネーション作成

--- app/controllers/pharmacist_profiles_controller.rb
+++ app/controllers/pharmacist_profiles_controller.rb
@@ -37,7 +37,7 @@ class PharmacistProfilesController < ApplicationController
 
   def search
     @q = PharmacistProfile.ransack(params[:q])
-    @results = @q.result
+    @results = @q.result.page(params[:page]).per(10)
   end
 
 
