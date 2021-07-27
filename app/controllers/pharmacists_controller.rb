ec958ab4b194d2a40057f59a0c24ce15a427efd3
ryota-5k <rk0509@hotmail.co.jp>
2021-07-27 16:30:41 +0900

    学生マイページ作成

--- app/controllers/pharmacists_controller.rb
+++ app/controllers/pharmacists_controller.rb
@@ -2,12 +2,11 @@ class PharmacistsController < ApplicationController
   def show
     @pharmacist_profile = current_pharmacist.pharmacist_profile
     if current_pharmacist.students
-      @students = current_pharmacist.students
       if current_pharmacist.relationships.find_by(allow: true)
-        @approved_students = @students.joins(:relationships).where(relationships: { allow: true })
+        @approved_students = Student.eager_load(:relationships, :student_profile).where(relationships: { pharmacist_id: current_pharmacist.id, allow: true })
       end
       if current_pharmacist.relationships.find_by(allow: false)
-        @not_approved_students = @students.joins(:relationships).where(relationships: { allow: false })
+        @not_approved_students = Student.eager_load(:relationships, :student_profile).where(relationships: { pharmacist_id: current_pharmacist.id, allow: false })
       end
     end
   end
