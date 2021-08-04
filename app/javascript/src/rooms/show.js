b39b53158148e23e55adca61b53ba98cb0af3a34
ryota-5k <rk0509@hotmail.co.jp>
2021-08-04 14:11:49 +0900

    メッセージ送信欄の修正、rspec修正

--- app/javascript/src/rooms/show.js
+++ app/javascript/src/rooms/show.js
@@ -1,8 +1,16 @@
 document.addEventListener("turbolinks:load", function(){
   scrollSet()
+  document.querySelectorAll('.message_area').forEach(expandArea)
 });
 
 function scrollSet(){
   let talkRoomContent = document.getElementById('talk_room_content');
   talkRoomContent.scrollIntoView(false);
 };
+
+function expandArea(el){
+  let dummy = el.querySelector('.dummy_message')
+  el.querySelector('#message_content').addEventListener('input', e => {
+    dummy.textContent = e.target.value + '\u200b'
+  })
+}
