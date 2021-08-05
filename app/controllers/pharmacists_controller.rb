650e6fd28d6789a22cb11db8483057bcb0eb3786
ryota-5k <rk0509@hotmail.co.jp>
2021-08-05 18:16:44 +0900

    マイページのリファクタリング

--- app/controllers/pharmacists_controller.rb
+++ app/controllers/pharmacists_controller.rb
@@ -4,13 +4,8 @@ class PharmacistsController < ApplicationController
   def show
     @pharmacist_profile = current_pharmacist.pharmacist_profile
     @notifications = current_pharmacist.notifications
-    if current_pharmacist.students
-      if current_pharmacist.relationships.find_by(allow: true)
-        @approved_students = Student.eager_load(:relationships, :student_profile).where(relationships: { pharmacist_id: current_pharmacist.id, allow: true })
-      end
-      if current_pharmacist.relationships.find_by(allow: false)
-        @not_approved_students = Student.eager_load(:relationships, :student_profile).where(relationships: { pharmacist_id: current_pharmacist.id, allow: false })
-      end
-    end
+    @approved_students = Student.eager_load(:relationships, :student_profile).where(relationships: { pharmacist_id: current_pharmacist.id, allow: true })
+    current_pharmacist.relationships.find_by(allow: false)
+    @not_approved_students = Student.eager_load(:relationships, :student_profile).where(relationships: { pharmacist_id: current_pharmacist.id, allow: false })
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,13 +4,7 @@ class StudentsController < ApplicationController
   def show
     @student_profile = current_student.student_profile
     @notifications = current_student.notifications
-    if current_student.pharmacists
-      if current_student.relationships.find_by(allow: true)
-        @approved_pharmacists = Pharmacist.eager_load(:relationships, :pharmacist_profile).where(relationships: { student_id: current_student.id, allow: true })
-      end
-      if current_student.relationships.find_by(allow: false)
-        @not_approved_pharmacists = Pharmacist.eager_load(:relationships, :pharmacist_profile).where(relationships: { student_id: current_student.id, allow: false })
-      end
-    end
+    @approved_pharmacists = Pharmacist.eager_load(:relationships, :pharmacist_profile).where(relationships: { student_id: current_student.id, allow: true })
+    @not_approved_pharmacists = Pharmacist.eager_load(:relationships, :pharmacist_profile).where(relationships: { student_id: current_student.id, allow: false })
   end
 end
